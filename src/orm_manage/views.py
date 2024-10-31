1bf90b2f8a280a14b05533f5fa55763d884f5c5d
Kenneth Love <11908+kennethlove@users.noreply.github.com>
2024-10-31 14:00:19 -0700

    View expects authentication

--- src/orm_manage/views.py
+++ src/orm_manage/views.py
@@ -4,22 +4,24 @@ import os
 from django.conf import settings
 from django.core.management import call_command
 from django.http import HttpResponse
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework.permissions import IsAuthenticated
 
 ALLOW_LIST: list[str] = settings.MANAGE_ALLOW_LIST if hasattr(settings, "MANAGE_ALLOW_LIST") else []
 
 
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
 def run_command_view(request, command: str) -> HttpResponse:
     """Execute a management command from a list of allowed commands.
 
     Accepts a command name, authentication token, and optional arguments as a JSON body.
     """
 
-    token = request.headers.get("Authorization")
     body = json.loads(request.POST.get("data", "{}"))
 
     if command in ALLOW_LIST:
         data = " ".join([f"--{key} {value}" for key, value in body.items()])
-        command_to_run = f"python manage.py {command} {data}"
         call_command(command, **body)
         return HttpResponse("Command executed")
     return HttpResponse("Command not allowed", status=403)
