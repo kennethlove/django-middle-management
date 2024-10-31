58ab114e467bc231a8da6f41b16a2bde0c4127ff
Kenneth Love <11908+kennethlove@users.noreply.github.com>
2024-10-31 13:44:04 -0700

    Let's do this with `call_command` instead of PATH tomfoolery

--- src/orm_manage/views.py
+++ src/orm_manage/views.py
@@ -2,6 +2,7 @@ import json
 import os
 
 from django.conf import settings
+from django.core.management import call_command
 from django.http import HttpResponse
 
 ALLOW_LIST: list[str] = settings.MANAGE_ALLOW_LIST if hasattr(settings, "MANAGE_ALLOW_LIST") else []
@@ -18,7 +19,7 @@ def run_command_view(request, command: str) -> HttpResponse:
 
     if command in ALLOW_LIST:
         data = " ".join([f"--{key} {value}" for key, value in body.items()])
-        command_to_run = f"python ./manage.py {command} {data}"
-        os.system(command_to_run)
+        command_to_run = f"python manage.py {command} {data}"
+        call_command(command, **body)
         return HttpResponse("Command executed")
     return HttpResponse("Command not allowed", status=403)
--- tests/test_run_command_view.py
+++ tests/test_run_command_view.py
@@ -1,7 +1,9 @@
 from django.urls import reverse
+from unittest.mock import patch
 
 
-def test_basic_usage(client):
+@patch("orm_manage.views.call_command")
+def test_basic_usage(call_command, client):
     """A POST request with a valid header and command name should return a 200 response."""
     headers = {
         "HTTP_AUTHORIZATION": "Bearer some_token",
@@ -10,3 +12,4 @@ def test_basic_usage(client):
     url = reverse("manage_run_command", kwargs={"command": "check"})
     response = client.post(url, data=data, **headers)
     assert response.status_code == 200
+    assert call_command.called_once_with("check", **data)
