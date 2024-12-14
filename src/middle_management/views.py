d10d9f435609996484e3637032ef5aef76ff39ad
Kenneth Love <kennethlove@gmail.com>
2024-12-14 11:59:30 -0800

    Removed reliance on DRF

--- src/middle_management/views.py
+++ src/middle_management/views.py
@@ -1,16 +1,16 @@
 import json
 
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.core.management import call_command
 from django.http import HttpResponse
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from django.views.decorators.http import require_POST
 
 ALLOW_LIST: list[str] = settings.MANAGE_ALLOW_LIST if hasattr(settings, "MANAGE_ALLOW_LIST") else []
 
 
-@api_view(["POST"])
-@permission_classes([IsAuthenticated])
+@require_POST()
+@login_required
 def run_command_view(request, command: str) -> HttpResponse:
     """Execute a management command from a list of allowed commands.
 
